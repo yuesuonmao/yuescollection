2b8e507c6bffd55f0197baf3887631793b14d414
yuesuonmao <y.mao@erasolutions.us>
2024-11-07 14:09:13 -0500

    asd

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -139,7 +139,8 @@ const PlaceOrder = () => {
                     </div>
 
                     <div className='w-full text-end mt-8'>
-                        <button type='submit' className='bg-black text-white px-16 py-3 text-sm'>PLACE ORDER</button>
+                    <button type='submit' className='bg-black text-white px-16 py-3 text-sm rounded-md'>PLACE ORDER</button>
+
                     </div>
                 </div>
             </div>
